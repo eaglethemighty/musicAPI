b7441aef9eb047670dee69b2ee5be748a1be3af5
moszroute <67115774+amichalakpoppa@users.noreply.github.com>
2021-11-24 16:31:50 +0100

    Add dbcontext service with correct database connection string

--- MusicAPI/MusicAPIMVC/Program.cs
+++ MusicAPI/MusicAPIMVC/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MusicAPIMVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string ConnectionString = builder.Configuration.GetConnectionString("MusicDBAccess");
+
+builder.Services.AddDbContext<MusicDBAccess>(options => options.UseSqlServer(ConnectionString));
 
 var app = builder.Build();
 
