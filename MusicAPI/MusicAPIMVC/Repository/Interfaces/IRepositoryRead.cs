46d8e2fade32578c65c79f73003a9fd47e8bde90
moszroute <67115774+amichalakpoppa@users.noreply.github.com>
2021-11-24 18:46:26 +0100

    Create repository CRUD actions interfaces

---
+++ MusicAPI/MusicAPIMVC/Repository/Interfaces/IRepositoryRead.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicAPIMVC.Repository.Interfaces
+{
+    public interface IRepositoryRead<T>
+    {
+        public Task<T> GetByIdAsync(int id);
+        public T GetById(int id);
+        public Task<IEnumerable<T>> GetAllAsync();
+        public IEnumerable<T> GetAll();
+    }
+}
