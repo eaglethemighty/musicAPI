042ad514d41f6468187495c82df7c6284b3d1e32
moszroute <67115774+amichalakpoppa@users.noreply.github.com>
2021-11-24 16:31:16 +0100

    Create DB Context to access database

---
+++ MusicAPI/MusicAPIMVC/Data/MusicDBAccess.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicAPIMVC.Models;
+
+namespace MusicAPIMVC.Data
+{
+    public class MusicDBAccess : DbContext
+    {
+        public MusicDBAccess(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<PlaylistSong> PlaylistSongs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.SeedData();
+                
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
