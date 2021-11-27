fde18e42de9b2fcf01715649d1451478fd270fde
moszroute <67115774+amichalakpoppa@users.noreply.github.com>
2021-11-27 20:08:07 +0100

    Add Playlist sequence to represent song sequence in a playlist

---
+++ MusicAPI/MusicAPIMVC/Migrations/20211127182525_AddPlaylistSequence.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicAPIMVC.Migrations
+{
+    public partial class AddPlaylistSequence : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SequenceInPlaylist",
+                table: "PlaylistSongs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SequenceInPlaylist",
+                table: "PlaylistSongs");
+        }
+    }
+}
