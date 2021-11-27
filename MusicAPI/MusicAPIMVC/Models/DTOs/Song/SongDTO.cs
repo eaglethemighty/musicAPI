87adad742eeecb05ceacec05ff229897e793822b
moszroute <67115774+amichalakpoppa@users.noreply.github.com>
2021-11-27 11:22:59 +0100

    Create Song READ operation

---
+++ MusicAPI/MusicAPIMVC/Models/DTOs/Song/SongDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicAPIMVC.Models.DTOs.Song
+{
+    public class SongDTO
+    {
+        public string Name { get; set; }
+        public TimeSpan Length { get; set; }
+    }
+}
---
+++ MusicAPI/MusicAPIMVC/Models/Profiles/SongProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MusicAPIMVC.Models.DTOs.Song;
+
+namespace MusicAPIMVC.Models.Profiles
+{
+    public class SongProfile : Profile
+    {
+        public SongProfile()
+        {
+            CreateMap<Song, SongDTO>();
+        }
+    }
+}
